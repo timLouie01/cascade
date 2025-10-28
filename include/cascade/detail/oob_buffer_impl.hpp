0a4c2daf1d215341391f29bb5320b3e334b3d85a
Tim <156990499+timLouie01@users.noreply.github.com>
2025-10-28 03:43:21 -0400

    fix the chunk method

--- include/cascade/detail/oob_buffer_impl.hpp
+++ include/cascade/detail/oob_buffer_impl.hpp
@@ -230,12 +230,12 @@ template<typename... CascadeTypes>
 
 template<typename... CascadeTypes>
 size_t oob_send_buffer<CascadeTypes...>::get_fill_chunks() {
-    // volatile uint64_t* rdma_head_ptr = reinterpret_cast<volatile uint64_t*>(head.load());
-    volatile uint64_t* rdma_tail_ptr = reinterpret_cast<volatile uint64_t*>(tail.load());
+    volatile uint64_t* rdma_head_ptr = reinterpret_cast<volatile uint64_t*>(head.load());
+    // volatile uint64_t* rdma_tail_ptr = reinterpret_cast<volatile uint64_t*>(tail.load());
     volatile uint64_t* rdma_send_tail_ptr = reinterpret_cast<volatile uint64_t*>(send_tail.load());
 
-    // _mm_clflush(const_cast<const void*>(static_cast<volatile void*>(rdma_head_ptr)));
-    _mm_clflush(const_cast<const void*>(static_cast<volatile void*>(rdma_tail_ptr)));
+    _mm_clflush(const_cast<const void*>(static_cast<volatile void*>(rdma_head_ptr)));
+    // _mm_clflush(const_cast<const void*>(static_cast<volatile void*>(rdma_tail_ptr)));
     _mm_clflush(const_cast<const void*>(static_cast<volatile void*>(rdma_send_tail_ptr)));
     const uint64_t chunk_size = 5 * 1024; // 5 KiB
     // if (*rdma_send_tail_ptr >= *rdma_head_ptr){
@@ -247,12 +247,12 @@ size_t oob_send_buffer<CascadeTypes...>::get_fill_chunks() {
     // }
     // uint64_t bytes_in_buff = (*rdma_tail_ptr >= *rdma_head_ptr) ? (*rdma_tail_ptr - *rdma_head_ptr) : (ring_size - *rdma_head_ptr + *rdma_tail_ptr);
   
-     if (*rdma_send_tail_ptr >= *rdma_tail_ptr){
+     if (*rdma_send_tail_ptr >= *rdma_head_ptr){
         // Not Wrap
-        return (*rdma_send_tail_ptr - *rdma_tail_ptr)/chunk_size;
+        return (*rdma_send_tail_ptr - *rdma_head_ptr)/chunk_size;
     }else{
         // Wrap
-        return (ring_size - *rdma_tail_ptr)/chunk_size + (*rdma_send_tail_ptr)/chunk_size;;
+        return (ring_size - *rdma_head_ptr)/chunk_size + (*rdma_send_tail_ptr)/chunk_size;
     }
 
     // return bytes_in_buff / chunk_size;
